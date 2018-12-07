ef7943d09a6abcd78fc040ea98a5e0fae0b9541c
Honza Bittner <git@honzabittner.cz>
2018-12-07 20:15:21 +0100

    Fix circle, remove not used methods from Canvas

--- app/canvas.py
+++ app/canvas.py
@@ -23,31 +23,6 @@ class Canvas(QtWidgets.QWidget):
         else:
             self._brush = None
 
-    def _add_command(self, start_x: int, start_y: int, end_x: int, end_y: int):
-        shape_command = self._brush.shape(
-            self._controller,
-            start_x,
-            start_y,
-            end_x,
-            end_y
-        )
-        self._controller.execute_command(shape_command)
-
-    def _complete_command(self, start_x: int, start_y: int, end_x: int, end_y: int):
-        self._add_command(start_x, start_y, end_x, end_y)
-        self._start = None
-
-    def _pointDistance(self, p1, p2):
-        return math.floor(
-            math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2))
-
-    def _lerp(self, v0, v1, i):
-        return v0 + i * (v1 - v0)
-
-    def _getEquidistantPoints(self, p1, p2):
-        n = self._pointDistance(p1, p2)
-        return [(self._lerp(p1[0], p2[0], 1./n*i), self._lerp(p1[1], p2[1], 1./n*i)) for i in range(n+1)]
-
     # -------------------------- QWidget overridden methods ----------------------------
 
     # Overriding paintEvent method of QWidget to respond to QEvent.Paint.
--- app/commands.py
+++ app/commands.py
@@ -130,10 +130,10 @@ class PrintRectCommand(Command):
 class PrintCircleCommand(Command):
     def __init__(self, receiver, start_x: int, start_y: int, end_x: int, end_y: int, color: Tuple[int, int, int]):
         super().__init__(receiver)
-        center = ((start_x + end_x) // 2, (start_y + end_y) // 2)
+        center = (start_x, start_y)
         # Classic formula for distance of two points
         diameter = math.floor(
-            math.sqrt((start_x - end_x) ** 2 + (start_y - end_y) ** 2)
+            math.sqrt((start_x - end_x) ** 2 + (start_y - end_y) ** 2) * 2
         )
         self.circle = Circle(
             Point(*center),
